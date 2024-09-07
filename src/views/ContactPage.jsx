88a49c4c800e84d045f6fdae07c21e490445bd42
YeMyat144 <149359862+YeMyat144@users.noreply.github.com>
2024-09-07 11:43:51 +0700

    later change

---
+++ src/views/ContactPage.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { AppBar, Toolbar, Typography, TextField, Grid2, Card, CardMedia, CardContent, Button } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles({
+    root: {
+      flexGrow: 1,
+    },
+    searchBar: {
+      margin: '20px 0',
+    },
+    card: {
+      maxWidth: 345,
+    },
+    media: {
+      height: 140,
+    },
+  });
+
+function ContactPage() {
+    const classes = useStyles();
+    return (
+       <div className={classes.root}>
+          <AppBar position="static">
+            <Toolbar>
+              <Link to="/" style={{ color: '#fff', textDecoration: 'none', marginRight: '20px' }}>Home</Link>
+              <Link to="/about" style={{ color: '#fff', textDecoration: 'none', marginRight: '20px' }}>About</Link>
+              <Link to="/contact" style={{ color: '#fff', textDecoration: 'none' }}>Contact</Link>
+            </Toolbar>
+          </AppBar>
+        <div>
+            <p>This is contact page</p>
+        </div>
+    </div>
+    );
+}
+export default ContactPage;
