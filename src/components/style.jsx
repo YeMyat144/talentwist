4d2476607653e973a5aa34f7e2e9e5eabfc16732
YeMyat144 <149359862+YeMyat144@users.noreply.github.com>
2024-10-13 13:54:29 +0700

    Added search bar layout and styling

--- src/components/style.jsx
+++ src/components/style.jsx
@@ -1,11 +1,17 @@
+import { AlignVerticalCenter } from "@mui/icons-material";
 import { colors } from "@mui/material";
+import { display, margin, width } from "@mui/system";
 
 const styles = {
     root: {
       flexGrow: 1,
     },
     searchBar: {
-      margin: '20px 56px',
+      margin: '20px auto',
+      display: 'flex',
+      width: '40%',
+      justifycontent: 'center',
+      alignitem: 'center',
     },
     card: {
       width: 354,
